62f991d34aeb5548887de77ed8bbfa70ae7cfc35
Flarespark Developer <dev@flarespark.com>
2026-03-10 11:44:39 +0100

    ✨ Restore /connect route and WalletController

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\InvestmentController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\WalletController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -21,6 +22,9 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/connect', [WalletController::class, 'index'])->name('connect');
+Route::post('/connect', [WalletController::class, 'connect'])->name('connect.submit');
 
 // Health check endpoint for monitoring
 Route::get('/health', function () {
