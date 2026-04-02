f33fb873d9d576db45dfea6cefe4ec7474d578a2
Flarespark Developer <dev@flarespark.com>
2026-04-02 22:36:07 +0100

    Add missing guest middleware - fixes login redirect

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         
         $middleware->alias([
             'admin' => \App\Http\Middleware\Admin::class,
+            'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         ]);
         
         // Exempt wallet connection route from CSRF for static HTML form
